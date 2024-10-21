90fb6fb76a9d3077a49103dcff665f37aa9f4a63
MekhakGhapantsyan <mekhakghapantsyan@gmail.com>
2024-10-21 15:19:47 +0400

    initial setup graphql.

---
+++ Birthday/Birthday/GraphQL/Client/AppoloClient.swift
@@ -0,0 +1,12 @@
+//
+//  AppoloClient.swift
+//  Birthday
+//
+//  Created by MEKHAK GHAPANTSYAN on 21.10.24.
+//
+
+import Foundation
+import Apollo
+
+let apolloClient = ApolloClient(url: URL(string: "https://birthdayapp.store/graphql")!)
+
