06f301c5757fd3fc213d112e17e9ad055d2b4442
hakobyaann <109814279+hakobyaann@users.noreply.github.com>
2024-10-23 11:45:25 +0400

    fixed
    Indentation in affected files.
    Separate property.

--- Birthday/Birthday/Models/LandingButton.swift
+++ Birthday/Birthday/Models/LandingButton.swift
@@ -9,40 +9,40 @@ import SwiftUI
 
 struct LandingButton: View {
 
-    var title: String
-    var textColor: Color
-    var backgroundColor: Color
-    var action: () -> Void
-    var cornerRadii: [CGFloat]
+  var title: String
+  var textColor: Color
+  var backgroundColor: Color
+  var action: () -> Void
+  var cornerRadii: [CGFloat]
 
-    var body: some View {
-        Button(action: action) {
-            HStack {
-                Spacer()
-                Text(title)
-                    .font(.system(size: 20))
-                    .padding()
-                    .foregroundColor(textColor)
-                Spacer()
-            }
-            .background(backgroundColor)
-            .clipShape(
-                .rect(
-                    topLeadingRadius: cornerRadii[0],
-                    bottomLeadingRadius: cornerRadii[1],
-                    bottomTrailingRadius: cornerRadii[2],
-                    topTrailingRadius: cornerRadii[3]
-                )
-            )
-        }
+  var body: some View {
+    Button(action: action) {
+      HStack {
+        Spacer()
+        Text(title)
+          .font(.system(size: 20))
+          .padding()
+          .foregroundColor(textColor)
+        Spacer()
+      }
+      .background(backgroundColor)
+      .clipShape(
+        .rect(
+          topLeadingRadius: cornerRadii[0],
+          bottomLeadingRadius: cornerRadii[1],
+          bottomTrailingRadius: cornerRadii[2],
+          topTrailingRadius: cornerRadii[3]
+        )
+      )
     }
+  }
 
 }
 
 #Preview {
-    LandingButton(title: String.Button.register,
-                  textColor: .lightPink,
-                  backgroundColor: .darkRed,
-                  action: { },
-                  cornerRadii: [42, 0, 42, 42])
+  LandingButton(title: String.Button.register,
+                textColor: .lightPink,
+                backgroundColor: .darkRed,
+                action: { },
+                cornerRadii: [42, 0, 42, 42])
 }
--- Birthday/Birthday/Screens/LandingView.swift
+++ Birthday/Birthday/Screens/LandingView.swift
@@ -9,38 +9,44 @@ import SwiftUI
 
 struct LandingView: View {
 
-    var body: some View {
-        ZStack {
-            Color.lightPink
-                .ignoresSafeArea()
-            VStack {
-                Image(.birth)
-                    .imageScale(.large)
-                VStack(spacing: 8) {
-                    LandingButton(
-                        title: String.Button.signIn,
-                        textColor: .darkRed,
-                        backgroundColor: .mainPink,
-                        action: { print("Navigation to Sign in page") },
-                        cornerRadii: [42, 0, 42, 42]
-                    )
-                    LandingButton(
-                        title: String.Button.register,
-                        textColor: .mainPink,
-                        backgroundColor: .darkRed,
-                        action: { print("Navigation to Register page") },
-                        cornerRadii: [42, 42, 42, 0]
-                    )
-                }
-                .padding(.vertical, 50)
-            }
-            .padding(.horizontal, 67)
-        }
+  var body: some View {
+    ZStack {
+      Color.lightPink
+        .ignoresSafeArea()
+      EnteranceView()
+        .padding(.horizontal, 67)
     }
+  }
 
 }
 
+struct EnteranceView: View {
 
+  var body: some View {
+    VStack {
+      Image(.birth)
+        .imageScale(.large)
+      VStack(spacing: 8) {
+        LandingButton(
+          title: String.Button.signIn,
+          textColor: .darkRed,
+          backgroundColor: .mainPink,
+          action: { print("Navigation to Sign in page") },
+          cornerRadii: [42, 0, 42, 42]
+        )
+        LandingButton(
+          title: String.Button.register,
+          textColor: .mainPink,
+          backgroundColor: .darkRed,
+          action: { print("Navigation to Register page") },
+          cornerRadii: [42, 42, 42, 0]
+        )
+      }
+      .padding(.vertical, 50)
+    }
+  }
+
+}
 #Preview {
-    LandingView()
+  LandingView()
 }
