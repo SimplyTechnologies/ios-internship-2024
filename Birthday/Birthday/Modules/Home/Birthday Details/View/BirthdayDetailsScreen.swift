a4b40a3a5491cf1d603712f22a783c2f53e7ee5c
Narek <68569879+Aslanyan99n@users.noreply.github.com>
2024-10-30 17:20:19 +0400

    feature/change-password // Change password (#25)
    Add change password

--- Birthday/Birthday/Modules/Home/Birthday Details/View/BirthdayDetailsScreen.swift
+++ Birthday/Birthday/Modules/Home/Birthday Details/View/BirthdayDetailsScreen.swift
@@ -204,9 +204,7 @@ extension BirthdayDetailsScreen {
     Button {
       guard let id = viewModel.birthdayData.id else { return }
       viewModel.deleteBirthDay(id: id) {
-        DispatchQueue.main.async {
-          router.pop()
-        }
+        router.pop()
       }
     } label: {
       Image(.delete)
--- Birthday/Birthday/Modules/Tabbar/View/TabBarView.swift
+++ Birthday/Birthday/Modules/Tabbar/View/TabBarView.swift
@@ -100,7 +100,8 @@ extension TabBarView {
               model: profileModel) {
                 doneAction()
               }
-          case .changePassword: Text("Change password")
+          case let .changePassword(viewModel):
+            ChangePasswordScreen(viewModel: viewModel)
           }
         }
     }
@@ -181,12 +182,12 @@ extension TabBarView {
   enum ProfileScreens: Hashable {
     
     case editProfile(viewModel: EditAccountViewModel, profileModel: ProfileModel, doneAction: () -> Void)
-    case changePassword
+    case changePassword(viewModel: ChangePasswordViewModel)
     
     var id: UUID {
       switch self {
       case let .editProfile(viewModel, _, _): viewModel.id
-      case .changePassword: UUID()
+      case let .changePassword(viewModel): viewModel.id
       }
     }
     
@@ -198,7 +199,8 @@ extension TabBarView {
       switch self {
       case let .editProfile(viewModel, _, _):
         hasher.combine(viewModel.id)
-      case .changePassword: hasher.combine(UUID())
+      case let .changePassword(viewModel):
+        hasher.combine(viewModel.id)
       }
     }
     
